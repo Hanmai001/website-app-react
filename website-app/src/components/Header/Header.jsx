2472711bf1159515d44abface0c03bd486df7eee
Hanmai001 <88010632+Hanmai001@users.noreply.github.com>
2022-07-03 13:16:00 +0700

    header

---
+++ website-app/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './Header.module.css';
+function Header() {
+    return <header className={styles.header}>
+        <h1>DONGREVIEW</h1>
+        <input placeholder='Tìm kiếm'></input>
+        <ul className={styles.nav}>
+            <li style={{fontSize: '1.8rem'}}>
+                PHIM ĐỀ XUẤT
+            </li>
+            <li>
+                Phim mới
+            </li>
+            <li>
+                Thể loại
+            </li>
+            <li>
+                Quốc gia
+            </li>
+            <li>
+                Phim lẻ
+            </li>
+            <li>
+                Phim bộ
+            </li>
+            <li>
+                Phim chiếu rạp
+            </li>
+        </ul>   
+    </header>
+}
+
+export default Header;
