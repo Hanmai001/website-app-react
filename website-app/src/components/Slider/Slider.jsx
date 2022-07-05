5edabc8406a900fbb266f91a47f3e9cae3a4f85a
Hanmai001 <88010632+Hanmai001@users.noreply.github.com>
2022-07-05 14:09:10 +0700

    modify slider

--- website-app/src/components/Slider/Slider.jsx
+++ website-app/src/components/Slider/Slider.jsx
@@ -3,15 +3,17 @@ import Picture from "../Picture/Picture";
 import styles from "./Slider.module.css";
 import pics from "./pics";
 
+const checks = [0, 1, 2];
 function Slider() {
     const [pos, setPos] = React.useState(0);
-    const [check, setCheck] = React.useState([true, false, false]);
     function increase(){
         if (pos === 2) {
+            console.log('render increase1');
             setPos(0);
         }
         else {
             setPos(pos + 1);
+            console.log(pos, 'pos');
         }
     }
     function decrease(){
@@ -19,36 +21,27 @@ function Slider() {
             setPos(2);
         }
         else {
-            setPos(pos - 1);
+             setPos(pos - 1);
         }
     }
-    function updateCheck(index) {
-        setCheck((prevCheck) => {
-            for(var i = 0; i < 3; i++) {
-                if (prevCheck[i] === true) prevCheck[i] = false;
-            }
-            prevCheck[index] = true;
-            return [...prevCheck];
-        })
-    }
     return <div className={styles.slider}>
-        <button onClick={() => { decrease(); updateCheck(pos);}} className={styles.buttonl}>
+        <button onClick={decrease} className={styles.buttonl} value={pos}>
             <i className="ti-angle-left"></i>
         </button>
-        <button onClick={() => { increase(); updateCheck(pos);}} className={styles.buttonr}>
+        <button onClick={increase} className={styles.buttonr} value={pos}>
             <i className="ti-angle-right"></i>
         </button>
-        {/*
-        {pics.map((pic) => (<Picture src={pic.src} title={pic.title}/>))}
-        */}
         <Picture src={pics[4 * pos].src} title={pics[4 * pos].title}/>
         <Picture src={pics[4 * pos + 1].src} title={pics[4 * pos + 1].title}/>
         <Picture src={pics[4 * pos + 2].src} title={pics[4 * pos + 2].title}/>
         <Picture src={pics[4 * pos + 3].src} title={pics[4 * pos + 3].title}/>
         <div className={styles.dot}>
+            {/* 
             <span className={check[0] ? styles.sdot : styles.ndot}></span>
             <span className={check[1] ? styles.sdot : styles.ndot}></span>
             <span className={check[2] ? styles.sdot : styles.ndot}></span>
+            */}
+            {checks.map((check, index) => (<span className={index === pos ? styles.sdot : styles.ndot}></span>))}
         </div>
         
     </div>
