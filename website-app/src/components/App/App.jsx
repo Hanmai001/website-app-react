c04b7d41bbee68a1eee6493083b8af6b4007c14a
Hanmai001 <88010632+Hanmai001@users.noreply.github.com>
2022-07-27 14:18:59 +0700

    link signup/forgotpassw layout

--- website-app/src/components/App/App.jsx
+++ website-app/src/components/App/App.jsx
@@ -10,24 +10,27 @@ import ReviewPage from "../ReviewPage/ReviewPage";
 //Member Profile
 import MemberPage from "../ProfilePage/MemberPage";
 import AdminPage from "../ProfilePage/AdminPage";
-import SignUp from "../SignUp/SignUp"
+import SignUp from "../SignUp/SignUp";
 import ForgotPassword from "../ForgotPassword/ForgotPassword";
 
 function App() {
   return (
-      <ForgotPassword/>
-      // <SignUp/>
-    // <Router>
-    //   <Fragment>
-    //     <div>
-    //       <Routes>
-    //         <Route exact path="/" element={<HomePage />}></Route>
-    //         <Route exact path="/introfilm" element={<Introfilm />}></Route>
-    //         <Route exact path="/reviewpage" element={<ReviewPage />}></Route>
-    //       </Routes>
-    //     </div>
-    //   </Fragment>z
-    // </Router>
+    <Router>
+      <Fragment>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<HomePage />}></Route>
+            <Route exact path="/introfilm" element={<Introfilm />}></Route>
+            <Route exact path="/reviewpage" element={<ReviewPage />}></Route>
+            <Route exact path="/signup" element={<SignUp />}></Route>
+            <Route exact path="/admin" element={<AdminPage />}></Route>
+            <Route exact path="/member" element={<MemberPage />}></Route>
+            <Route exact path="/forgotpassword" element={<ForgotPassword />}></Route>
+          </Routes>
+        </div>
+      </Fragment>
+      z
+    </Router>
   );
 }
 
