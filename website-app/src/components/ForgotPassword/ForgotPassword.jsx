aff7b3cd327b346fa338887aba30086399c663ec
Hanmai001 <88010632+Hanmai001@users.noreply.github.com>
2022-07-28 14:23:59 +0700

    modify signup/forgotpass

--- website-app/src/components/ForgotPassword/ForgotPassword.jsx
+++ website-app/src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import styles from './ForgotPassword.module.css';
+import React from "react";
+import styles from "./ForgotPassword.module.css";
 import clsx from "clsx";
 
-function ForgotPassword(){
-    return(
-        <div className={styles.modal}>
-            <div className={styles.modalContainer}>
-                <button className={styles.btn}>
-                    <i className="ti-close"></i>
-                </button>
-                <div className={styles.head}><h2>QUÊN MẬT KHẨU</h2></div>
-                <p style={{marginTop: "4rem"}}>Tên đăng nhập</p>
-                <input type = "text" placeholder=" Tên đăng nhập" style={{paddingLeft: "0.5rem"}} ></input>
-                <p style={{marginTop: '2rem'}}>Mật khẩu gần nhất</p>
-                <input type="password" placeholder=" Mật khẩu gần nhất" style={{paddingLeft: "0.5rem"}}></input>
-                <p style={{marginTop: '2rem'}}>Số điện thoại xác nhận</p>
-                <input type="text" placeholder="Số điện thoại" style={{paddingLeft: "0.5rem"}}></input>
-                <div className={styles.forgot}>
-                    <button>
-                        <a href="/">Lấy mật khẩu</a>
-                    </button>
-                </div>
-                <div className={styles.alter}>
-                    <a href="/">Đăng nhập</a>/ <a href="/">Đăng kí</a> 
-                </div>
-            </div>
-        </div>)
+function ForgotPassword() {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalContainer}>
+        <div className={styles.head}>
+          <h2>QUÊN MẬT KHẨU</h2>
+        </div>
+        <p style={{ marginTop: "4rem" }}>Tên đăng nhập</p>
+        <input
+          type="text"
+          placeholder=" Tên đăng nhập"
+          style={{ paddingLeft: "0.5rem", paddingRight: "0.5rem" }}
+        ></input>
+        <p style={{ marginTop: "2rem" }}>Nhập mật khẩu mới</p>
+        <input
+          type="password"
+          placeholder="Nhập mật khẩu mới"
+          style={{ paddingLeft: "0.5rem", paddingRight: "0.5rem" }}
+        ></input>
+        <p style={{ marginTop: "2rem" }}>Xác nhận lại mật khẩu</p>
+        <input
+          type="text"
+          placeholder="Xác nhận mật khẩu"
+          style={{ paddingLeft: "0.5rem", paddingRight: "0.5rem" }}
+        ></input>
+        <div className={styles.forgot}>
+          <button>
+            <a href="/">Đồng ý</a>
+          </button>
+        </div>
+        <div className={styles.alter}>
+          <a href="/">Đăng nhập</a>/ <a href="/signup">Đăng kí</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ForgotPassword
+export default ForgotPassword;
